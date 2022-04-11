9f227a96db9ab6bf8e80167ec2cfbbe97f705acf
Azizbek <azizbekbakhodirov3147@gmail.com>
2022-04-11 17:38:43 +0500

    deploying to heroku2

--- src/main/java/uz/master/warehouse/properties/ServerProperties.java
+++ src/main/java/uz/master/warehouse/properties/ServerProperties.java
@@ -18,6 +18,6 @@ public class ServerProperties {
     private String protocol;
 
     public String getServerUrl() {
-        return this.protocol + "://" + this.ip + ":" + this.port;
+        return this.protocol + "://" + this.ip+"/" /*+ ":" + this.port*/;
     }
 }
